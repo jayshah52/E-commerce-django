336895e5ac71ed87e966592c8d48fe7cf5d18258
Jay Shah <sjay5200@gmail.com>
2021-04-30 02:08:53 +0530

    Login and Logout functionality

--- ecomsite/store/urls.py
+++ ecomsite/store/urls.py
@@ -1,10 +1,14 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from . import views
+# from .views import CustomLoginView
 
 urlpatterns = [
 	#Leave as empty string for base url
 	path('', views.store, name="store"),
+	path("login/", views.login_request, name="login"),
+path("logout", views.logout_request, name="logout"),
 	path('cart/', views.cart, name="cart"),
 	path('checkout/', views.checkout, name="checkout"),
 	path('update_item/', views.updateItem, name = "update_item"),
